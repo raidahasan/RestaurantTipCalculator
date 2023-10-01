52550caa126fa83966c8eedf58efba3cfe32b29b
Raida Hasan <raidahasan09@gmail.com>
2023-10-01 17:43:37 -0400

    final

--- src/TipCalculator.java
+++ src/TipCalculator.java
@@ -14,12 +14,13 @@ public class TipCalculator {
         //scan.nextLine();
         double cost = 0;
         double total = 0;
+        //makes the code repeat to ask for all the items ordered until -1 ia entered
         while (cost != -1) {
-            System.out.println("Enter the price of your item (enter -1 when done): ");
+            System.out.print("Enter the price of your item (enter -1 when done): $");
             cost = scan.nextDouble();
             total = total + cost;
             scan.nextLine();
-            if (cost==-1){
+            if (cost==-1){  //makes sure -1 isn't added to the cost
                 total++;
             }
             if(cost != -1){
@@ -29,11 +30,18 @@ public class TipCalculator {
                 System.out.println("  ");
             }
         }
+        //calculating all the variables needed
         double totalTip = (percentage/100.0)*total;
         double billWithTip = totalTip + total;
         double perPersonCost = total/ numberOfPeople;
         double tipPerPerson = totalTip/numberOfPeople;
         double totalCostPerPerson = billWithTip/numberOfPeople;
+        double totalWithTax = total*1.08875;
+        double totalCostWithTax = totalWithTax + totalTip;
+        double totalCostPerPersonWithTax = totalCostWithTax/numberOfPeople;
+        //making sure they are all rounded to the nearest two decimal points
+        total = Math.round((total*100));
+        total/= 100;
         totalTip = Math.round((totalTip*100));
         totalTip /=100;
         billWithTip = Math.round((billWithTip*100));
@@ -44,18 +52,58 @@ public class TipCalculator {
         tipPerPerson /=100;
         totalCostPerPerson= Math.round((totalCostPerPerson*100));
         totalCostPerPerson /=100;
+        totalCostWithTax= Math.round((totalCostWithTax*100));
+        totalCostWithTax /=100;
+        totalCostPerPersonWithTax= Math.round((totalCostPerPersonWithTax*100));
+        totalCostPerPersonWithTax /=100;
+        //prints all values
         System.out.println("--------------------------------------------");
-        System.out.println("Total bill before tip: $" + total);
+        if ((total*10)%1 == 0){
+            System.out.println("Total bill before tip: $" + total + "0");
+        }else{
+            System.out.println("Total bill before tip: $" + total);
+        }
         System.out.println("Total percentage: " + percentage + "%");
-        System.out.println("Total tip: $" + totalTip);
-        System.out.println("Total bill with tip: $" + billWithTip);
-        System.out.println("Per person cost before tip: $" + perPersonCost);
-        System.out.println("Tip per person: $" + tipPerPerson);
-        System.out.println("Total cost per person: $" + totalCostPerPerson);
+        if ((totalTip*10)%1 == 0){
+            System.out.println("Total tip: $" + totalTip + "0");
+        }else{
+            System.out.println("Total tip: $" + totalTip);
+        }
+        if ((billWithTip*10)%1 == 0){
+            System.out.println("Total bill with tip: $" + billWithTip + "0");
+        }else{
+            System.out.println("Total bill with tip: $" + billWithTip);
+        }
+        if ((perPersonCost*10)%1 == 0){
+            System.out.println("Per person cost before tip: $" + perPersonCost + "0");
+        }else{
+            System.out.println("Per person cost before tip: $" + perPersonCost);
+        }
+        if ((tipPerPerson*10)%1 == 0){
+            System.out.println("Tip per person: $" + tipPerPerson + "0");
+        }else{
+            System.out.println("Tip per person: $" + tipPerPerson);
+        }
+        if ((totalCostPerPerson*10)%1 == 0){
+            System.out.println("Total cost per person: $" + totalCostPerPerson + "0");
+        }else{
+            System.out.println("Total cost per person: $" + totalCostPerPerson);
+        }
+        if ((totalCostWithTax*10)%1 == 0){
+            System.out.println("Total cost with tax : $" +totalCostWithTax + "0");
+        }else{
+            System.out.println("Total cost with tax : $" +totalCostWithTax);
+        }
+        if ((totalCostPerPersonWithTax*10)%1 == 0){
+            System.out.println("Total cost per person with tax : $" +totalCostPerPersonWithTax);
+        }else{
+            System.out.println("Total cost per person with tax : $" +totalCostPerPersonWithTax);
+        }
         System.out.println("--------------------------------------------");
-
-        System.out.println(items);
-
+        //prints list of all items ordered
+        System.out.println("Items ordered: ");
+        for (int i = 0; i < items.size(); i++)
+            System.out.println(items.get(i));
     }
 
 
--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -2,8 +2,5 @@ public class TipCalculatorRunner {
     public static void main(String[] args){
 
         TipCalculator tip = new TipCalculator();
-
-
-
     }
 }
